535ecc104498123b34cc462a0444f722d3cc6dab
EnYousef <97245529+EnYousef@users.noreply.github.com>
2023-07-07 19:10:10 +0300

    edit

--- src/components/usefulComponents/Name.js
+++ src/components/usefulComponents/Name.js
@@ -7,7 +7,7 @@ export function Name({ title, classes = '' }) {
     }
     return (
         <div className={`text-4xl sm:text-7xl font-bold -tracking-normal title ${classes}`}>
-            <h1 className={`${typing} dark:after:bg-[#172735] dark:after:md:bg-[#192835] dark:after:lg:bg-[#232938] after:sm:bg-[#e8eff5] after:md:bg-[#e7eef5]`} onAnimationEnd={onAnimationEnd}>{title}</h1>
+            <h1 className={`${typing} dark:after:bg-[#172735] dark:after:md:bg-[#192835] dark:after:lg:bg-[#232938] after:sm:bg-[#e8eff5] after:bg-[#e8eff5] after:md:bg-[#e7eef5]`} onAnimationEnd={onAnimationEnd}>{title}</h1>
         </div>
     );
 }
